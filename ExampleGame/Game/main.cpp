a2c81502b0338ddb3cdb059cce98f6a88a669c62
AlpCanNalbant <enteldantel95@gmail.com>
2024-02-26 08:31:50 +0300

    Window title changed to "Game Sample".

--- ExampleGame/Game/main.cpp
+++ ExampleGame/Game/main.cpp
@@ -3,7 +3,7 @@
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *args[])
 {
 	Wcm::Log->OutputFile = Wcm::GetBaseDirectory() / "ExampleGame.log";
-	App editor({.title = "Splash Screen", .width = 1280, .height = 800, .borderless = false});
+	App editor({.title = "Game Sample", .width = 1280, .height = 800, .borderless = false});
 
 	editor.Run();
 
